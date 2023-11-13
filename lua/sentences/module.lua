e52f71a6b3b2ab8764e9cc6e6f5e8c15867c1949
cubicruler <alexander.lastovski@gmail.com>
2023-11-13 14:55:42 +0100

    move back to original position

--- lua/sentences/module.lua
+++ lua/sentences/module.lua
@@ -4,8 +4,8 @@ local api = vim.api
 local M = {}
 
 M.highlight_sentence = function()
-  -- Get the current line and cursor position
-  local line = api.nvim_get_current_line()
+  -- Get cursor position
+  local row = api.nvim_win_get_cursor(0)[1]
   local col = api.nvim_win_get_cursor(0)[2]
 
   -- Find the start and end of the sentence using sentence movement commands
@@ -21,10 +21,10 @@ M.highlight_sentence = function()
   api.nvim_buf_clear_namespace(buf, -1, 0, -1)
 
   -- Add new highlight
-  api.nvim_buf_add_highlight(buf, -1, "Error", api.nvim_win_get_cursor(0)[1] - 1, start_col - 1, end_col)
+  api.nvim_buf_add_highlight(buf, -1, "Error", row - 1, start_col - 1, end_col)
 
   -- Move cursor to its original position
-  -- api.nvim_win_set_cursor(0, {line, col})
+  api.nvim_win_set_cursor(0, {row, col})
 end
 
 return M
